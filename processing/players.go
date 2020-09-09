b78df8bb43f890f3b4ef42821ff85f06fcdbcdab
Cufee <vkouzin@outlook.com>
2020-09-09 15:39:37 -0700

    Now calculating tank battles and rating in a separate func

--- processing/players.go
+++ processing/players.go
@@ -1,6 +1,7 @@
 package processing
 
 import (
+	"errors"
 	"log"
 	"math"
 	"time"
@@ -82,89 +83,34 @@ func calcPlayerRating(playerData mongo.Player, playersChannel chan mongo.Player,
 	defer wg.Done()
 	defer func() {
 		playersChannel <- playerData
-		// // Add playerData to the channel and finish waitgroup
-		// if playerData.SessionBattles > 0 {
-		// 	log.Println("Sent to channel")
-		// }
 	}()
-	// Used at the bottom to calculate session rating
-	oldBattles := playerData.Battles
 
-	// // Check current player battles
-	// liveBattles, err := wgapi.GetLiveBattles(playerData.ID)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	if oldBattles == 0 {
+	// Get live vehicle stats
+	vehicles, err := wgapi.GetVehicleStats(playerData.ID)
+	if err != nil {
+		log.Println(err)
 		playerData.SessionRating = 0
 		playerData.SessionBattles = 0
 		return
 	}
-	vehicles, err := wgapi.GetVehicleStats(playerData.ID)
+
+	// Calcualte Raw rating and get total battles
+	battles, rawRating, err := CalcVehicleRawRating(vehicles)
 	if err != nil {
 		log.Println(err)
-		playerData.SessionRating = -1
-		playerData.SessionBattles = -1
-		return
+		playerData.AverageRating = 0
+		playerData.SessionRating = 0
+		playerData.SessionBattles = 0
 	}
 
-	var battles float64
-	var rawRating float64
-
-	for _, tank := range vehicles {
-		filter := bson.M{"tank_id": tank.TankID}
-		tankAvgData, err := mongo.GetTankAvg(filter)
-		if err != nil {
-			// No tank average data, no need to spam log/report
-			log.Println("No average for", tank.TankID)
-			continue
-		}
-		if tankAvgData.All.Battles == 0 || tank.All.Battles == 0 {
-			log.Println("Bad average data for", tank.TankID)
-			continue
-		}
-
-		// Expected values for WN8
-		expDef := tankAvgData.All.DroppedCapturePoints / tankAvgData.All.Battles
-		expFrag := tankAvgData.Special.KillsPerBattle
-		expSpot := tankAvgData.Special.SpotsPerBattle
-		expDmg := tankAvgData.Special.DamagePerBattle
-		expWr := tankAvgData.Special.Winrate
-
-		// Actual performance
-		pDef := tank.All.DroppedCapturePoints / tank.All.Battles
-		pFrag := tank.All.Frags / tank.All.Battles
-		pSpot := tank.All.Spotted / tank.All.Battles
-		pDmg := tank.All.DamageDealt / tank.All.Battles
-		pWr := tank.All.Wins / tank.All.Battles * 100
-
-		// Calculate WN8 metrics
-		rDef := pDef / expDef
-		rFrag := pFrag / expFrag
-		rSpot := pSpot / expSpot
-		rDmg := pDmg / expDmg
-		rWr := pWr / expWr
-
-		adjustedWr := math.Max(0, ((rWr - 0.71) / (1 - 0.71)))
-		adjustedDmg := math.Max(0, ((rDmg - 0.22) / (1 - 0.22)))
-		adjustedDef := math.Max(0, (math.Min(adjustedDmg+0.1, (rDef-0.10)/(1-0.10))))
-		adjustedSpot := math.Max(0, (math.Min(adjustedDmg+0.1, (rSpot-0.38)/(1-0.38))))
-		adjustedFrag := math.Max(0, (math.Min(adjustedDmg+0.2, (rFrag-0.12)/(1-0.12))))
-
-		rating := math.Round(((980 * adjustedDmg) + (210 * adjustedDmg * adjustedFrag) + (155 * adjustedFrag * adjustedSpot) + (75 * adjustedDef * adjustedFrag) + (145 * math.Min(1.8, adjustedWr))))
-
-		ratingWeighted := rating * tank.All.Battles
-		battles += tank.All.Battles
-		rawRating += ratingWeighted
-	}
+	oldBattles := playerData.Battles
 
 	if int(battles) < oldBattles {
 		log.Println("Current battles cnt is less than old battles cnt for", playerData.Nickname)
 		playerData.Battles = int(battles)
 		playerData.SessionRating = 0
 		playerData.SessionBattles = 0
+		// Update player record
 		_, err := mongo.UpdatePlayer(playerData, false)
 		if err != nil {
 			log.Println(err)
@@ -174,11 +120,17 @@ func calcPlayerRating(playerData mongo.Player, playersChannel chan mongo.Player,
 
 	// oldBattles defined at the start of this func
 	playerData.SessionBattles = int(battles) - oldBattles
-	if playerData.SessionBattles == 0 {
-		// log.Println("Session battles is 0 for", playerData.Nickname)
+	if oldBattles == 0 || playerData.SessionBattles == 0 {
+		playerData.AverageRating = int(math.Round(rawRating / battles))
 		playerData.SessionRating = 0
+		// Update player record
+		_, err := mongo.UpdatePlayer(playerData, false)
+		if err != nil {
+			log.Println(err)
+		}
 		return
 	}
+
 	oldRating := playerData.AverageRating
 	playerData.AverageRating = int(math.Round(rawRating / battles))
 	playerData.Battles = int(battles)
@@ -186,6 +138,81 @@ func calcPlayerRating(playerData mongo.Player, playersChannel chan mongo.Player,
 	sessionRating := sessionRatingWeighted / (int(battles) - oldBattles)
 	playerData.SessionRating = sessionRating
 
-	// log.Println(playerData.SessionBattles, playerData.SessionRating, playerData.Nickname)
 	return
+}
+
+// CalcVehicleRawRating - Calculate rating for a slice of VehicleStats structs.
+func CalcVehicleRawRating(vehicles []wgapi.VehicleStats) (battles float64, rawRating float64, err error) {
+	var wg sync.WaitGroup
+	if len(vehicles) == 0 {
+		return 0, 0, errors.New("VehicleStats slice empty")
+	}
+	// Calculate rating for all vehicles in go routines
+	// Create channels for battles and rawRating
+	var battlesChan chan float64
+	var rawRatingChan chan float64
+	for _, tank := range vehicles {
+		wg.Add(1)
+		go func(tank wgapi.VehicleStats) {
+			defer wg.Done()
+			filter := bson.M{"tank_id": tank.TankID}
+			tankAvgData, err := mongo.GetTankAvg(filter)
+			if err != nil {
+				// No tank average data, no need to spam log/report
+				log.Println("No average for", tank.TankID)
+				return
+			}
+			if tankAvgData.All.Battles == 0 || tank.All.Battles == 0 {
+				log.Println("Bad average data for", tank.TankID)
+				return
+			}
+
+			// Expected values for WN8
+			expDef := tankAvgData.All.DroppedCapturePoints / tankAvgData.All.Battles
+			expFrag := tankAvgData.Special.KillsPerBattle
+			expSpot := tankAvgData.Special.SpotsPerBattle
+			expDmg := tankAvgData.Special.DamagePerBattle
+			expWr := tankAvgData.Special.Winrate
+
+			// Actual performance
+			pDef := tank.All.DroppedCapturePoints / tank.All.Battles
+			pFrag := tank.All.Frags / tank.All.Battles
+			pSpot := tank.All.Spotted / tank.All.Battles
+			pDmg := tank.All.DamageDealt / tank.All.Battles
+			pWr := tank.All.Wins / tank.All.Battles * 100
+
+			// Calculate WN8 metrics
+			rDef := pDef / expDef
+			rFrag := pFrag / expFrag
+			rSpot := pSpot / expSpot
+			rDmg := pDmg / expDmg
+			rWr := pWr / expWr
+
+			adjustedWr := math.Max(0, ((rWr - 0.71) / (1 - 0.71)))
+			adjustedDmg := math.Max(0, ((rDmg - 0.22) / (1 - 0.22)))
+			adjustedDef := math.Max(0, (math.Min(adjustedDmg+0.1, (rDef-0.10)/(1-0.10))))
+			adjustedSpot := math.Max(0, (math.Min(adjustedDmg+0.1, (rSpot-0.38)/(1-0.38))))
+			adjustedFrag := math.Max(0, (math.Min(adjustedDmg+0.2, (rFrag-0.12)/(1-0.12))))
+
+			rating := math.Round(((980 * adjustedDmg) + (210 * adjustedDmg * adjustedFrag) + (155 * adjustedFrag * adjustedSpot) + (75 * adjustedDef * adjustedFrag) + (145 * math.Min(1.8, adjustedWr))))
+
+			ratingWeighted := rating * tank.All.Battles
+			battlesChan <- tank.All.Battles
+			rawRatingChan <- ratingWeighted
+		}(tank)
+	}
+	wg.Wait()
+	// Close channels
+	close(battlesChan)
+	close(rawRatingChan)
+
+	// Sum battles and rating
+	for b := range battlesChan {
+		battles += b
+	}
+	for r := range rawRatingChan {
+		rawRating += r
+	}
+
+	return battles, rawRating, nil
 }
